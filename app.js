a6af6201c2e99469b8c0f03492376d0fa0fb534a
jimschaeffer <130175038+jimschaeffer@users.noreply.github.com>
2023-07-11 09:24:56 -0500

    finished exercise

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 // TODO: Enable strict mode
 
+"use strict";
+
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
-  // TODO: Add a try/catch block to
-  // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
+
+function parseToJSON(data) {
+    // TODO: Add a try/catch block to
+  try{
+      // attempt to convert 'data' to JSON
+    return JSON.parse(data);
+  } catch(error) {
+    // TODO: if an exception is raised
+    // print the error to the console
+    // and return null
+    console.error(error);
+    return null;
+  }
 }
 
 let failData = "<h1>Hello World!</h1>"
@@ -18,3 +25,7 @@ let passData = JSON.stringify({ success: true });
 console.log(parseToJSON(failData));
 
 console.log(parseToJSON(passData));
+
+// CONSOLE:
+// null
+// { success: true }
